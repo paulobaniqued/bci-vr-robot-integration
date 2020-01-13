47ce2dad989273fb975d6e1f07407f93e45f9266
Paul Dominick Baniqued <47854500+paulbaniqued@users.noreply.github.com>
2020-01-13 16:40:30 +0000

    Add files via upload

--- test_bandpass.py
+++ test_bandpass.py
@@ -9,13 +9,6 @@ fs = 500.0
 lowcut = 7.5
 highcut = 11.5
 
-def butter_bandpass(lowcut, highcut, fs, order=4):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    sos = butter(order, [low, high], analog=False, btype='band', output='sos')
-    return sos
-
 def butter_lowpass(lowcut, highcut, fs, order=4):
     nyq = 0.5 * fs
     low = lowcut / nyq
@@ -35,11 +28,6 @@ def zero_order_butterworth(data, order, fs, low_cutoff, high_cutoff):
     x = butter_lowpass_filter(data, 7.5, 11.5, fs, order=4)
     return x
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
-    sos = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = sosfilt(sos, data)
-    return y
-
 # Load training data
 training_data = np.load('training_data_raw_1.npy')
 training_data = np.float32(training_data)
@@ -47,28 +35,38 @@ print(training_data)
 print(np.shape(training_data))
 print(training_data.dtype)
 
-trial_number = 65
+trial_number = 56
 #choose from 0 - 79
 
 trial = training_data[trial_number,:,1:5000]
 print(trial)
 print(np.shape(trial))
 
+# Plotting Raw
 f, ax = plt.subplots()
 x_point = np.arange(4999)
 ax.plot(x_point, trial[0,:])
+ax.plot(x_point, trial[1,:])
+ax.plot(x_point, trial[2,:])
+ax.plot(x_point, trial[3,:])
+ax.plot(x_point, trial[4,:])
+ax.plot(x_point, trial[5,:])
+ax.plot(x_point, trial[6,:])
+ax.plot(x_point, trial[7,:])
 plt.show()
-
-# Filter
-#eeg_filtered = butter_bandpass_filter(trial, lowcut, highcut, fs, order=4)
-#print("Mu signal: ")
-#print(eeg_filtered)
 
 eeg_filtered = zero_order_butterworth(trial, 4, fs, lowcut, highcut)
 print(eeg_filtered)
 
-# Plotting Raw
+# Plotting Filtered
 f, ax = plt.subplots()
 x_point = np.arange(4999)
 ax.plot(x_point, eeg_filtered[0,:])
+ax.plot(x_point, eeg_filtered[1,:])
+ax.plot(x_point, eeg_filtered[2,:])
+ax.plot(x_point, eeg_filtered[3,:])
+ax.plot(x_point, eeg_filtered[4,:])
+ax.plot(x_point, eeg_filtered[5,:])
+ax.plot(x_point, eeg_filtered[6,:])
+ax.plot(x_point, eeg_filtered[7,:])
 plt.show()
