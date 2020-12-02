8ff4c4e1835eba428edc64aff8bc1b467be3adc5
Paul Dominick Baniqued <47854500+paulbaniqued@users.noreply.github.com>
2020-12-02 19:08:50 +0000

    Update TriggerFeedback.cs

--- control-unity-vr/Assets/Scripts/TriggerFeedback.cs
+++ control-unity-vr/Assets/Scripts/TriggerFeedback.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LSL;
 
 public class TriggerFeedback : MonoBehaviour
 {
     public ParticleSystem SparkleLeft;
     public ParticleSystem SparkleRight;
+    public string StreamType = "feedback";
+    liblsl.StreamInfo[] streamInfos;
+    liblsl.StreamInlet streamInlet;
+    float[] sample;
+    private int channelCount = 0;
 
-    // Update is called once per frame
     void Update()
     {
+        // Manual Feedback Trigger
         if (Input.GetKeyDown("up")) // Correct Feedback Right
         {
             Debug.Log("trigger up key down");
@@ -24,6 +30,25 @@ public class TriggerFeedback : MonoBehaviour
             SparkleLeft.Play();
 
         }
-        
+
+        // LSL Feedback Trigger
+        if (streamInlet == null)
+        {
+            streamInfos = liblsl.resolve_stream("type", StreamType, 1, 0.0);
+            if (streamInfos.Length > 0)
+            {
+                streamInlet = new liblsl.StreamInlet(streamInfos[0]);
+                channelCount = streamInlet.info().channel_count();
+                streamInlet.open_stream();
+            }
+        }
+
+        if (streamInlet != null)
+        {
+            sample = new float[channelCount];
+            double lastTimeStamp = streamInlet.pull_sample(sample, 0.0f);
+            Debug.Log(sample[0]);
+
+        }
     }
 }
